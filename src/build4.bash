438ca8f589f9b2568d70265f6bb96870bee9a23e
Jaid <jaid.jsx@gmail.com>
2022-10-27 01:45:45 +0200

    Small syntax change
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/build4.bash
+++ src/build4.bash
@@ -2,4 +2,4 @@
 set -o errexit -o pipefail
 
 platform=$(uname --hardware-platform)
-CARGO_INSTALL_ROOT=/build RUSTFLAGS='-C target-feature=+crt-static -C opt-level=z -C debuginfo=0 -C codegen-units=1' /root/.cargo/bin/cargo install --target "$platform"-unknown-linux-gnu sd
+CARGO_INSTALL_ROOT=/build RUSTFLAGS='-C target-feature=+crt-static -C opt-level=z -C debuginfo=0 -C codegen-units=1' /root/.cargo/bin/cargo install --target "$platform-unknown-linux-gnu" sd
