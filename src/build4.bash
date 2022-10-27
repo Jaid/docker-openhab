7bdd81ca7096f8622c0252c042889c515417c91e
Jaid <jaid.jsx@gmail.com>
2022-10-27 05:35:01 +0200

    Improved Rust compilation
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/build4.bash
+++ src/build4.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail
 
-platform=$(uname --hardware-platform)
-CARGO_INSTALL_ROOT=/build RUSTFLAGS='-C target-feature=+crt-static -C opt-level=z -C debuginfo=0 -C codegen-units=1' /root/.cargo/bin/cargo install --target "$platform-unknown-linux-gnu" sd
+rustTarget=$(rustc -vV | sed -n 's|host: ||p')
+CARGO_INSTALL_ROOT=/build RUSTFLAGS='-C target-feature=+crt-static -C opt-level=z -C debuginfo=0 -C codegen-units=1' /root/.cargo/bin/cargo install --target "$rustTarget" sd
