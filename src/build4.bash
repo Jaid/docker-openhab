a06a3929b826ef5a1e804a867cf60c1faf8af965
Jaid <jaid.jsx@gmail.com>
2022-10-27 05:44:21 +0200

    Fixed path
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/build4.bash
+++ src/build4.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail
 
-rustTarget=$(rustc -vV | sed -n 's|host: ||p')
+rustTarget=$(/root/.cargo/bin/rustc -vV | sed -n 's|host: ||p')
 CARGO_INSTALL_ROOT=/build RUSTFLAGS='-C target-feature=+crt-static -C opt-level=z -C debuginfo=0 -C codegen-units=1' /root/.cargo/bin/cargo install --target "$rustTarget" sd
